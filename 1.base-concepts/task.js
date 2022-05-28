87ecd7be7223449dcdc8ca906f50dab04f076f50
DrNickker <mintagirov2015@yandex.ru>
2022-05-29 01:38:31 +0500

    123

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,30 +1,32 @@
 "use strict";
 
-console.log ("Задание №1");
-
 function solveEquation(a, b, c) {
   let arr = [];
   let D = Math.pow(b, 2) - (4 * a * c);
   let x1, x2;
   
 if (D > 0) {
-  console.log("Дискриминант: " + D + " > 0, => 2 корня: ");
+  //console.log("Дискриминант: " + D + " > 0, => 2 корня: ");
   x1 = (-b + Math.sqrt(D)) / (2 * a);
   x2 = (-b - Math.sqrt(D)) / (2 * a);
   arr.push(x1.toFixed(0));
   arr.push(x2.toFixed(0));
   console.log(arr);
-} else if (D==0) {
-  console.log("Дискриминант: " + D + " = 0, => 1 корень: ");
+
+} else if (D===0) {
+  //console.log("Дискриминант: " + D + " = 0, => 1 корень: ");
   x1 = (-b) / (2 * a);
   arr.push(x1.toFixed(0));
   console.log(parseInt(arr[0]));
+
 } else {
-  console.log("Дискриминант: " + D + " < 0, => корней нет: ");
+  //console.log("Дискриминант: " + D + " < 0, => корней нет: ");
   console.log(arr);
 }
 return arr;
 };
+
+solveEquation(1, 4, 5)
 
 
 
@@ -32,10 +34,8 @@ return arr;
 
 /*  if(D < 0)
   return false;
-
   if(D == 0)
       arr = (-b/(2*a));
-
   else if(D > 0){
       let tmp = [];
       tmp.push((-b + Math.sqrt(d) )/(2*a));
